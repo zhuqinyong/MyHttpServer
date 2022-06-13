16dda896d341ddd942ea163ae79087871a5afcde
zhuqinyong <18326657779@163.com>
2022-06-13 14:51:54 +0800

    提交服务器代码

--- src/me/tsinyong/HttpServerStarter.java
+++ src/me/tsinyong/HttpServerStarter.java
@@ -1,16 +1,55 @@
 package me.tsinyong;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.util.CharsetUtil;
+
+import java.net.URI;
 import java.util.Properties;
 
 public class HttpServerStarter {
     private final static Properties PROPERTIES = new Properties();
 
-    public void port() {
-        PROPERTIES.put("port", 9090);
+    public static void start() {
+        EventLoopGroup master = new NioEventLoopGroup();
+        EventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            new ServerBootstrap().group(master, worker).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel socketChannel) {
+                    ChannelPipeline channelPipeline = socketChannel.pipeline();
+                    channelPipeline.addLast("httpServerCodec", new HttpServerCodec());
+                    channelPipeline.addLast("httpAggregator", new HttpObjectAggregator(512 * 1024));
+                    channelPipeline.addLast("httpServerHandler", new SimpleChannelInboundHandler<FullHttpRequest>() {
+                        @Override
+                        protected void channelRead0(ChannelHandlerContext channelHandlerContext, FullHttpRequest request) throws Exception {
+                            Channel channel = channelHandlerContext.channel();
+                            URI uri = new URI(request.uri());
+                            System.out.println(uri.getPath());
+                            System.out.println(request.method().name());
+                            System.out.println(request.content().toString());
+                            ByteBuf content = Unpooled.copiedBuffer("收到请求", CharsetUtil.UTF_8);
+                            channelHandlerContext.writeAndFlush(content);
+                            channelHandlerContext.channel().close();
+
+                        }
+                    });
+                }
+            }).bind((Integer) PROPERTIES.getOrDefault("port", 9090)).channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void start()
-    {
-
+    public static void port() {
+        PROPERTIES.put("port", 9090);
     }
 }
